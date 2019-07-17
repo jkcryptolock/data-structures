5961a61e8ae4a81ea6ebd4c08fe551e228482e41
jkcryptolock <jkconno@gmail.com>
2019-07-17 16:46:46 -0500

    Completed Stack and Queue with functional instantiation pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,34 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
+  
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.dequeue = function() {
+    var shifted = storage[1];
+    delete storage[1];
+    if (counter > 0) { 
+      counter--; 
+    }
+    var copy = {};
+    for (var key in storage) {
+      var currentKey = key;
+      var newKey = key - 1;
+      copy[newKey] = storage[currentKey];
+    }
+    storage = copy;
+    return shifted;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    counter ++;
+    storage[counter] = value;
   };
 
   someInstance.pop = function() {
+    var popped = storage[counter];
+    delete storage[counter];
+    if (counter > 0) { 
+      counter--; 
+    }
+    return popped;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
