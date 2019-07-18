dfbb3035c1eb1bf1f67a9e09b598eab5464491d3
jkcryptolock <jkconno@gmail.com>
2019-07-17 19:31:14 -0500

    implemented prototypal instantiation pattern for stack and queue problems

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,38 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+
+  someInstance.storage = {};
+  someInstance.counter = 0;
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+  },
+
+  dequeue: function() {
+    var shifted = this.storage[1];
+    delete this.storage[1];
+    if (this.counter > 0) { 
+      this.counter--; 
+    }
+    var copy = {};
+    for (var key in this.storage) {
+      var currentKey = key;
+      var newKey = key - 1;
+      copy[newKey] = this.storage[currentKey];
+    }
+    this.storage = copy;
+    return shifted;
+  },
+
+  size: function() {
+    return this.counter;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+
+  someInstance.storage = {};
+  someInstance.counter = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+  },
+
+  pop: function() {
+    var popped = this.storage[this.counter];
+    delete this.storage[this.counter];
+    if (this.counter > 0) {
+      this.counter--
+    }
+    return popped;
+  },
+
+  size: function() {
+    return this.counter;
+  }
+};
 
 
