8ad96f1f0986f2e8fbb02c9bd2d4e2728c291047
jkcryptolock <jkconno@gmail.com>
2019-07-17 19:55:11 -0500

    Completed the implementation of es6 instantiation pattern for stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
+  }
+  enqueue(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+  }
+  dequeue() {
+    var shifted = this.storage[1];
+    delete this.storage[1];
+    if (this.counter > 0) {
+      this.counter--;
+    }
+    var copy = {};
+    for (var key in this.storage) {
+      var currentKey = key;
+      var newKey = key - 1;
+      copy[newKey] = this.storage[currentKey];
+    }
+    this.storage = copy;
+    return shifted;
+  }
+  size() {
+    return this.counter;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,21 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
+  }
+  push(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+  }
+  pop() {
+    var popped = this.storage[this.counter];
+    delete this.storage[this.counter];
+    if (this.counter > 0) { this.counter-- }
+    return popped;
+  }
+  size() {
+    return this.counter;
   }
 
 }
