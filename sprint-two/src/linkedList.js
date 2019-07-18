6e956e911d5f3e15d99842073f85c6be052a6c6b
jkcryptolock <jkconno@gmail.com>
2019-07-18 18:54:26 -0500

    Completed functional instantiation pattern implementation of linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,15 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (!list.head) { 
+      list.head = newNode;
+    }
+    if (list.tail) { 
+      list.tail.next = newNode; 
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var headValue = list.head.value;
+    list.head = list.head.next;
+    return headValue;
   };
 
   list.contains = function(target) {
+    function findTarget(node) {
+      if (node.value === target) {
+        return true;
+      }
+      if (node.next === null) {
+        return false;
+      }
+      return findTarget(node.next);
+    }
+    return findTarget(list.head);
   };
 
   return list;
+  
 };
 
 var Node = function(value) {
