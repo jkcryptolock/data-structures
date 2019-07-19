3a70a817f694f8a81c16d32180177f65a5cdcbc0
jkcryptolock <jkconno@gmail.com>
2019-07-19 14:19:52 -0500

    Completed prototypal implementation of set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage[item]) {
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (var key in this._storage) {
+    if (key === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage[item]) {
+    delete this._storage[item]
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ - Constant, because we are doing direct value lookups - O(1)
  */
