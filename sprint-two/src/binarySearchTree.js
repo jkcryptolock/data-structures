39b15e9796a87505402ab0afa0efa1986437e5a2
Gibran Iqbal <gibran.iqbal91@gmail.com>
2019-07-20 02:08:54 -0500

    ESLint edits

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,66 +1,66 @@
 var BinarySearchTree = function(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  };
+  this.value = value;
+  this.left = null;
+  this.right = null;
+};
   
-  BinarySearchTree.prototype.insert = function(value) {
-    var twig = new BinarySearchTree(value);
+BinarySearchTree.prototype.insert = function(value) {
+  var twig = new BinarySearchTree(value);
   
-    if (twig.value < this.value) {
-      if (this.left === null) {
-        this.left = twig;
-        return this;
-      } else {
-        this.left.insert(value);
-      }
+  if (twig.value < this.value) {
+    if (this.left === null) {
+      this.left = twig;
+      return this;
+    } else {
+      this.left.insert(value);
     }
+  }
   
-    if (twig.value > this.value) {
-      if (this.right === null) {
-        this.right = twig;
-        return this;
-      } else {
-        this.insert.call(this.right, value);
-      }
+  if (twig.value > this.value) {
+    if (this.right === null) {
+      this.right = twig;
+      return this;
+    } else {
+      this.insert.call(this.right, value);
     }
-  };
+  }
+};
   
-  BinarySearchTree.prototype.contains = function(value) {
-    if (this.value === value) {
-      return true;
-    }
+BinarySearchTree.prototype.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  }
   
-    if (value < this.value) {
-      if (this.left !== null) {
-        return this.contains.call(this.left, value);
-      } else {
-        return false;
-      }
-    }
-  
-    if (value > this.value) {
-      if (this.right !== null) {
-        return this.contains.call(this.right, value);
-      } else {
-        return false;
-      }
-    }
-  };
-  
-  BinarySearchTree.prototype.depthFirstLog = function(cb) {
-    this.value = cb(this.value);
-    
+  if (value < this.value) {
     if (this.left !== null) {
-      this.depthFirstLog.call(this.left, cb);
+      return this.contains.call(this.left, value);
+    } else {
+      return false;
     }
-    
+  }
+  
+  if (value > this.value) {
     if (this.right !== null) {
-      this.depthFirstLog.call(this.right, cb);
+      return this.contains.call(this.right, value);
+    } else {
+      return false;
     }
+  }
+};
+  
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  this.value = cb(this.value);
     
-    return this;
-  };
+  if (this.left !== null) {
+    this.depthFirstLog.call(this.left, cb);
+  }
+    
+  if (this.right !== null) {
+    this.depthFirstLog.call(this.right, cb);
+  }
+    
+  return this;
+};
 
 
 
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,61 +2,61 @@
 
 // Instantiate a new graph
 var Graph = function() {
-    var newGraph = Object.create(Graph.prototype);
+  var newGraph = Object.create(Graph.prototype);
 
-    newGraph.nodes = {};
-    newGraph.edges = {};
+  newGraph.nodes = {};
+  newGraph.edges = {};
 
-    return newGraph;
+  return newGraph;
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-    this.nodes[node] = node;
+  this.nodes[node] = node;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-    return this.nodes[node] ? true : false;
+  return this.nodes[node] ? true : false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-    if (this.contains(node)) {
-        delete this.nodes[node];
-        delete this.edges[node]
-    }
+  if (this.contains(node)) {
+    delete this.nodes[node];
+    delete this.edges[node];
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-    for (var key in this.edges) {
-        var searchTo = this.edges[key][0] === fromNode && this.edges[key][1] === toNode;
-        var searchFrom = this.edges[key][1] === fromNode && this.edges[key][0] === toNode;
-        if (searchTo || searchFrom) {
-            return true;
-        }
+  for (var key in this.edges) {
+    var searchTo = this.edges[key][0] === fromNode && this.edges[key][1] === toNode;
+    var searchFrom = this.edges[key][1] === fromNode && this.edges[key][0] === toNode;
+    if (searchTo || searchFrom) {
+      return true;
     }
-    return false;
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-    this.edges[fromNode] = [fromNode, toNode];
+  this.edges[fromNode] = [fromNode, toNode];
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-    if (this.hasEdge(fromNode, toNode)) {
-        delete this.edges[fromNode];
-    }
+  if (this.hasEdge(fromNode, toNode)) {
+    delete this.edges[fromNode];
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-    for (var node in this.nodes) {
-        cb(this.nodes[node]);
-    }
+  for (var node in this.nodes) {
+    cb(this.nodes[node]);
+  }
 };
 
 /*
